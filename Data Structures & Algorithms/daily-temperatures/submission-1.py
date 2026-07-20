30ff033dcb99f061136fd044d89db11cca3cd32d
Samarth <113123386+luciferthestar302@users.noreply.github.com>
2026-07-20 23:40:56 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        #Brute Force
+        n = len(temperatures)
+        res = [0] * n
+
+        for i in range(n):
+            for j in range(i+1, n):
+                if temperatures[j] > temperatures[i]:
+                    res[i] = j - i  #  distance
+                    break           #  stop at first warmer day
+                    
+        return res
+        
