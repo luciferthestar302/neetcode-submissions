c2e40536b268658ed14cdd780d0fc1035a6d0f4c
Samarth <113123386+luciferthestar302@users.noreply.github.com>
2026-08-31 20:30:57 +0530

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        #Brute Force approach(USING HASHMAP), time complexity: O(n), space: O(n)
+        # count = {}
+        # for num in nums:
+        #     count[num] = count.get(num,0) +1
+
+        #     if count[num]>1:
+        #         return num
+
+        #No space used: O(1), using Linkedlist cycle detection - Floyd's algorithm
+
+        slow = nums[0]
+        fast = nums[0]
+        #Phase1: Find the meeting point (Detecting whether the cycle exists or not)
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow==fast:
+                break
+        #Phase2: Finding the entry point of cycle
+        slow = nums[0]
+        while slow!=fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+
+        return slow #entry point is the answer
+        
+
+
+        
